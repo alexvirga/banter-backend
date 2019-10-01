b4cf96b5d33909745567daca32fda9e707433461
alexvirga <alexander.virga@gmail.com>
2019-10-01 01:55:10 -0400

    first heroku commit

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,15 @@ Bundler.require(*Rails.groups)
 
 module BanterApi
   class Application < Rails::Application
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins 'http://localhost:3000', 'https://evening-mountain-63500.herokuapp.com/' #replace this url with that of your own heroku client app
+        resource '*', :headers => :any, :methods => [:get, :post, :patch]
+      end
+    end
+
+
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
